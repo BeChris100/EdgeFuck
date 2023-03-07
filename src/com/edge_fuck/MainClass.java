936f63a6d118af243c32a4f69097884984740f9a
BeChris100 <bechris.100@outlook.com>
2023-03-07 16:56:53 +0100

    .gitignore Update (remove 'out')

--- src/com/edge_fuck/MainClass.java
+++ src/com/edge_fuck/MainClass.java
@@ -7,27 +7,6 @@ public class MainClass {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Number num = new Number() {
-            @Override
-            public int intValue() {
-                return 0;
-            }
-
-            @Override
-            public long longValue() {
-                return 0;
-            }
-
-            @Override
-            public float floatValue() {
-                return 0;
-            }
-
-            @Override
-            public double doubleValue() {
-                return 0;
-            }
-        };
     }
 
 }
